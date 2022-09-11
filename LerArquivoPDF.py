8dedd1bf5de6127091af877eec90ab719c79e16e
Luckas-Ferreira <luckas0ferreira321@gmail.com>
2022-09-11 09:05:26 -0300

    atualização

--- LerArquivoPDF.py
+++ LerArquivoPDF.py
@@ -1,10 +1,15 @@
 import PyPDF2
-from time import sleep
 
 arq_PDF = PyPDF2.PdfReader('2022.1.pdf', 'rb')
 number_of_pages = len(arq_PDF.pages)
 
-def mostrarPDF(arq_PDF, number_of_pages):
+def show_on_screen(arq_PDF, number_of_pages):
+    for c in range(0, number_of_pages):
+        pag  = arq_PDF.pages[c]
+        text = pag.extract_text()
+        print(text)
+
+def convert_PDF_to_TXT (arq_PDF, number_of_pages):
     for c in range(0, number_of_pages):
         arq_PDF   = open('2022.1.pdf', 'rb')
         readerPDF = PyPDF2.PdfFileReader(arq_PDF)
@@ -12,29 +17,27 @@ def mostrarPDF(arq_PDF, number_of_pages):
         text      = pageObj.extractText()
         arq_PDF.close()
 
-        file1=open(r"Arquivo_TXT.txt","a")
-        file1.writelines(text)
-        file1.close()
-mostrarPDF(arq_PDF, number_of_pages)
+        arq_TXT = open(r"Arquivo_TXT.txt","a")
+        arq_TXT.writelines(text)
+        arq_TXT.close()
+
+#convert_PDF_to_TXT(arq_PDF, number_of_pages)
+contador = 0
+with open('Arquivo_TXT.txt', 'r', encoding='utf-8') as arquivo:
+    arquivo = arquivo.readlines()
+
+for line in arquivo:
+    if 'ACURCIO CASTELO DAVID' in line:
+        contador += 1
+
+file = open("Arquivo_TXT.txt") 
+
+for pos, l_num in enumerate(file): 
+    specified_lines = [11] 
+    if pos in specified_lines: 
+        print(l_num)
+        specified_lines.append(6)
 
 
+#show_on_screen(arq_PDF, number_of_pages)
 
-
-def encontrarProfessor():
-    pag = arq_PDF.pages[0]
-    texto = pag.extract_text()
-    contador = 0
-    for c in texto:
-        if texto.find():
-            contador += 1
-
-def mostrarPDF(reader, number_of_pages):
-    for c in range(0, number_of_pages):
-        paginas = reader.pages[c]
-        text = paginas.extract_text()
-        arq_PDF.append(text)
-        print(f'\033[1;31m {c} \033[m')
-
-pagina = arq_PDF.pages[0]
-texte = pagina.extract_text()
-c = 'ACURCIO CASTELO DAVID'
