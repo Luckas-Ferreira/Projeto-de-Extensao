7ff0fe5322983b7d46b7bcb231fc7830510446b1
Luckas-Ferreira <luckas0ferreira321@gmail.com>
2022-09-12 15:16:34 -0300

    perfeito

--- teste.py
+++ teste.py
@@ -8,19 +8,3 @@ def quantidade_Vezes_Professores():
     with open('NomeDisciplina.txt', 'r', encoding='utf-8') as arquivo2:
         disciplina = arquivo2.readlines()
         arquivo2.close()
-
-    
-    def Disciplina():
-        discionario = {'luckas': 'pote', 'lorem': 'gato'}
-        for c in range(0, 4):
-            print(discionario.keys(c))
-        
-
-    '''dicionario = defaultdict(str) 
-    for c in professor:
-        dicionario[c] += Disciplina()
-
-    for k, v in dicionario.items():
-        print(k.replace('\n', '') , '\033[1;31m aparece:\033[m' , v,)'''
-
-quantidade_Vezes_Professores()
