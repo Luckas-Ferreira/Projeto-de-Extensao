d5f9a1c900bc0707fe687d925e192cd7ce587dee
Luckas-Ferreira <luckas0ferreira321@gmail.com>
2022-09-11 22:29:03 -0300

    alteração

--- LerArquivoPDF.py
+++ LerArquivoPDF.py
@@ -1,13 +1,7 @@
-import PyPDF2
+import PyPDF2, re
 
 arq_PDF = PyPDF2.PdfReader('2022.1.pdf', 'rb')
 number_of_pages = len(arq_PDF.pages)
-
-def show_on_screen(arq_PDF, number_of_pages):
-    for c in range(0, number_of_pages):
-        pag  = arq_PDF.pages[c]
-        text = pag.extract_text()
-        print(text)
 
 def convert_PDF_to_TXT (arq_PDF, number_of_pages):
     for c in range(0, number_of_pages):
@@ -21,23 +15,35 @@ def convert_PDF_to_TXT (arq_PDF, number_of_pages):
         arq_TXT.writelines(text)
         arq_TXT.close()
 
-#convert_PDF_to_TXT(arq_PDF, number_of_pages)
-contador = 0
+convert_PDF_to_TXT(arq_PDF, number_of_pages)
+
 with open('Arquivo_TXT.txt', 'r', encoding='utf-8') as arquivo:
     arquivo = arquivo.readlines()
 
-for line in arquivo:
-    if 'ACURCIO CASTELO DAVID' in line:
-        contador += 1
+def Mostrar_No_Terminal(arq_PDF, number_of_pages):
+    for c in range(0, number_of_pages):
+        pag  = arq_PDF.pages[c]
+        text = pag.extract_text()
+        print(text)
 
-file = open("Arquivo_TXT.txt") 
+def escrever_Nomes_Professores ():
+    lista_Temporaria = []
+    lista_Completa   = []
+    for linhas in range(len(arquivo)):
+        if 'Vagas Oferecidas' in arquivo[linhas]:
+            lista_Temporaria.append(arquivo[linhas - 1])
 
-for pos, l_num in enumerate(file): 
-    specified_lines = [11] 
-    if pos in specified_lines: 
-        print(l_num)
-        specified_lines.append(6)
+    for pagina in range(len(lista_Temporaria)):
+        lista_Temporaria[pagina] = lista_Temporaria[pagina].rstrip('\n')
+        if 'Página' in lista_Temporaria[pagina]:
+            lista_Completa.append(lista_Temporaria[pagina - 1])
+
+    ordem = sorted(lista_Completa)
+    print(ordem)
+    '''NomesProfessores = open(r"NomesProfessores.txt","w")
+    NomesProfessores.writelines(lista_Completa)
+    NomesProfessores.close()'''
 
 
-#show_on_screen(arq_PDF, number_of_pages)
+convert_PDF_to_TXT(arq_PDF, number_of_pages)
 
