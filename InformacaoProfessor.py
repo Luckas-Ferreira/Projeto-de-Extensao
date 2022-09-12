ba85233859bd9da38de45a5b3335142d30ae1a64
Luckas-Ferreira <luckas0ferreira321@gmail.com>
2022-09-12 07:57:36 -0300

    teste

--- InformacaoProfessor.py
+++ InformacaoProfessor.py
@@ -1,4 +1,28 @@
-class HorarioAulas:
-    def __init__(self):
-        pass
-    
+with open('NomesProfessores.txt', 'r', encoding='utf-8') as arquivo2:
+    nomes = arquivo2.readlines()
+    arquivo2.close()
+
+a = []
+contador = 0
+nomes_Sem_Duplicatas = []
+
+for nome in nomes:
+    if nome not in nomes_Sem_Duplicatas:
+        nomes_Sem_Duplicatas.append(nome)
+print(nomes_Sem_Duplicatas)
+'''for todosNomes in range(0, len(nomes)):
+    for SemDuplicatas in range(0, len(nomes_Sem_Duplicatas)):
+        if nomes[todosNomes].count(SemDuplicatas):
+            a.append(nomes[todosNomes])
+
+for SemDuplicatas in  range(len(nomes_Sem_Duplicatas)):
+    nomes_Sem_Duplicatas[SemDuplicatas].count(str('ADRIANO CESAR ROSA DA COSTA'))'''
+ 
+'''
+tes = {}
+for pos, nome in enumerate(nomes_Sem_Duplicatas):
+    print(pos, nome)
+    if nome.count(nomes_Sem_Duplicatas):
+        contador += 1
+
+print(contador)'''
--- teste.py
+++ teste.py
@@ -1,24 +1,6 @@
-lista = ['Vagas Oferecidas: 50 Vagas Ocupadas: 17ZOTA087 - A MICROBIOLOGIA E IMUNOLOGIA\n',' Terça-feira 11:10 - 12:00\n', ' Quinta-feira 07:30 - 08:20\n', ' Quinta-feira 08:20 - 09:10\n', 'OSCAR BOAVENTURA NETO\n', 'Vagas Oferecidas: 50 Vagas Ocupadas: 27ZOTA089 - A ZOOLOGIA GERAL\n', 'CH:56 horas\n', ' Segunda-feira 09:20 - 10:10\n', ' Segunda-feira 10:10 - 11:00\n', ' Segunda-feira 11:10 - 12:00\n', 'OSCAR BOAVENTURA NETO\n', 'Vagas Oferecidas: 50 Vagas Ocupadas: 9ZOTA106 - A PARASITOLOGIA ZOOTÉCNICA\n', 'CH:54 horas\n', ' Quarta-feira 07:30 - 08:20\n', ' Quarta-feira 08:20 - 09:10\n', ' Quarta-feira 09:20 - 10:10\n', 'PATRICK HENRIQUE DA SILVA BRITO\n', 'Vagas Oferecidas: 60 Vagas Ocupadas: 43ZOTA073 - A INTRODUÇÃO À INFORMÁTICA\n', 'CH:54 horas\n', ' Terça-feira 10:10 - 11:00\n', ' Terça-feira 11:10 - 12:00\n', ' Terça-feira 12:00 - 12:50\n', '4 12/08/2022 08:48:39 PáginaTHIAGO ROCHA ARAUJO\n', ]
+lista = ['ACURCIO CASTELO DAVID', 'ADEMARIA APARECIDA DE SOUZA', 'ADRIANO CESAR ROSA DA COSTA' ]
 
-lista_Temporaria = []
-for alterar in range(len(lista)):
-    if 'Página' in lista[alterar]:
-        troca = lista[alterar].replace('Página', '\n')
-        lista_Temporaria.append(troca)
-    else:
-        lista_Temporaria.append(lista[alterar])
+for nome in range(len(lista)):
 
-
-
-
-# containing string = 'ber'
-try:
-    with open('NomesProfessores.txt', 'r') as fr:
-        lines = fr.readlines()
-  
-        with open('NomesProfessores.txt', 'w') as fw:
-            for line in lines:
-                if line.find(':') == -1:
-                    fw.write(line)
-except:
-    print("Oops! existe um erro")
+    print(f'{lista[nome]} aparece {ista.count(lista[nome])}')
+    
