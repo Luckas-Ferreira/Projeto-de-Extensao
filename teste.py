e3ed5ea57df684a968de8ec8ea28036642509c7a
Luckas-Ferreira <luckas0ferreira321@gmail.com>
2022-09-13 10:17:55 -0300

    a

--- teste.py
+++ teste.py
@@ -8,3 +8,26 @@ def quantidade_Vezes_Professores():
     with open('NomeDisciplina.txt', 'r', encoding='utf-8') as arquivo2:
         disciplina = arquivo2.readlines()
         arquivo2.close()
+
+    nome = ['ACURCIO CASTELO DAVID']
+    disciplina = ['A TEORIAS ORGANIZACIONAIS I']
+
+    s = [('rome', 1), ('paris', 2), ('paris', 'gato'), ('newyork', 3), ('paris', 4), ('delhi', 1)]
+    d = defaultdict(list)
+    for k, v in s:
+        d[k].append(v)
+    sorted(d.items())
+   
+    print(d)
+    
+    key_list = ['name', 'age', 'address']
+    value_list = ['Johnny', '27', 'New York']
+
+    dict_from_list = {}
+    for key in key_list:
+        for value in value_list:
+            dict_from_list[key] = value
+            break
+
+    #print(dict_from_list)
+quantidade_Vezes_Professores()
