0e5b5bcf88ceacaa091cbf2a7c62b01a0446bd3f
Luckas-Ferreira <luckas0ferreira321@gmail.com>
2022-09-21 04:32:49 -0300

    asd

--- Novo.py
+++ Novo.py
@@ -1,24 +1,16 @@
-def mostra_Todos_Cursos():
-    with open ('Arquivo_TXT.txt', 'r') as arquivo:
-        NomeCurso = arquivo.readlines()
-        arquivo.close()
+with open('DadosSeparados/NomesProfessores.txt', 'r', encoding='utf-8') as arquivo1:
+    professor = arquivo1.readlines()
+    arquivo1.close()
 
-    lista_Temporaria = []
-    for linha in range(len(NomeCurso)):
-        if 'Semestre' in NomeCurso[linha]:
-            lista_Temporaria.append(NomeCurso[linha].replace('- ', '\n', 2))
+with open('DadosSeparados/NomesDisciplina.txt',  'r', encoding='utf-8') as arquivo2:
+    disciplina = arquivo2.readlines()
+    arquivo2.close()
 
-    NomesCurso = open(r"DadosSeparados/NomesCurso.txt","w")
-    NomesCurso.writelines(lista_Temporaria)
-    NomesCurso.close()
-    with open ('DadosSeparados/NomesCurso.txt', 'r') as arquivo:
-        NomeCurso1 = arquivo.readlines()
-        arquivo.close()
+with open('DadosSeparados/CargaHoraria.txt',     'r', encoding='utf-8') as arquivo3:
+    horas = arquivo3.readlines()
+    arquivo3.close()
 
-    lista_Completa = []
-    for linha in range(len(NomeCurso1)):
-        if 'Semestre' in NomeCurso1[linha -1]:
-            lista_Completa.append(NomeCurso1[linha])
-    NomesCurso = open(r"DadosSeparados/NomesCurso.txt","w")
-    NomesCurso.writelines(lista_Completa)
-    NomesCurso.close()
+with open('DadosSeparados/NomesCurso.txt',       'r', encoding='utf-8') as arquivo4:
+    Curso = arquivo4.readlines()
+    arquivo4.close()
+
