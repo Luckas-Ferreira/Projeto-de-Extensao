ee285c5cfb6d2bad5b9e56db5a3b1d040ad9afd7
Luckas-Ferreira <luckas0ferreira321@gmail.com>
2022-09-21 05:47:05 -0300

    asdf

--- Novo.py
+++ Novo.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 with open('DadosSeparados/NomesProfessores.txt', 'r', encoding='utf-8') as arquivo1:
     professor = arquivo1.readlines()
     arquivo1.close()
@@ -14,3 +15,22 @@ with open('DadosSeparados/NomesCurso.txt',       'r', encoding='utf-8') as arqui
     Curso = arquivo4.readlines()
     arquivo4.close()
 
+nome_disciplina = list(zip(professor, disciplina))
+nome_horas      = list(zip(professor, horas))
+
+juntar = list(zip(nome_disciplina, horas))
+
+Lista_Final = defaultdict(list)
+for k, v in nome_disciplina:
+    Lista_Final[k].append(v)
+sorted(Lista_Final.items())
+
+Lista_horas = defaultdict(list)
+for k, v in nome_horas:
+    Lista_horas[k].append(v)
+sorted(Lista_horas.items())
+
+c = defaultdict(list)
+for k, v in Lista_Final.items():
+    c[k] = Lista_horas.keys()
+    print(c)
