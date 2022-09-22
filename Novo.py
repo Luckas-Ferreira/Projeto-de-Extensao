d011fc686702f0d35f8497904df7f580ebb14cbf
Luckas-Ferreira <luckas0ferreira321@gmail.com>
2022-09-21 21:35:35 -0300

    asdf

--- Novo.py
+++ Novo.py
@@ -17,12 +17,10 @@ with open('DadosSeparados/NomesCurso.txt',       'r', encoding='utf-8') as arqui
     Curso = arquivo4.readlines()
     arquivo4.close()
 
+
+
 nome_disciplina = list(zip(professor, disciplina))
 nome_horas      = list(zip(professor, horas))
-
-
-
-juntar = list(zip(nome_disciplina, horas))
 
 Lista_Final = defaultdict(list)
 for k, v in nome_disciplina:
@@ -33,13 +31,7 @@ Lista_horas = defaultdict(list)
 for k, v in nome_horas:
     Lista_horas[k].append(v)
 sorted(Lista_horas.items())
-
-Lista_curso = defaultdict(list)
-for k, v in nome_horas:
-    Lista_horas[k].append(v)
-sorted(Lista_horas.items())
-
-
+'''
 lfv = {}
 
 for k, v in enumerate(Lista_Final):
@@ -50,16 +42,48 @@ for k, v in enumerate(Lista_Final):
     lfv[v]["disciplinas"] = d
     lfv[v]['horas'] = h
 
-print(type(Curso))
-temporaria      = list(map(list, lfv.items()))
-curso_nome      = list(zip(Curso, temporaria))
-print(curso_nome)
-
-
-'''txtJson = json.dumps(lfv)
+txtJson = json.dumps(lfv)
 
 arquivoJson = open(r"DadosSeparados/ArquivoJson.json","w")
 arquivoJson.writelines(txtJson)
-
 arquivoJson.close()'''
 
+
+with open ('Arquivo_TXT.txt', 'r') as arquivo:
+    NomeCurso = arquivo.readlines()
+    arquivo.close()
+lista_Temporaria = []
+for linha in range(len(NomeCurso)):
+    if 'Semestre' in NomeCurso[linha]:
+        lista_Temporaria.append(NomeCurso[linha].replace('- ', '\n', 2))
+
+NomesCurso = open(r"DadosSeparados/NomesCurso.txt","w")
+NomesCurso.writelines(lista_Temporaria)
+NomesCurso.close()
+with open ('DadosSeparados/NomesCurso.txt', 'r') as arquivo:
+    NomeCurso1 = arquivo.readlines()
+    arquivo.close()
+
+lista_Completa = []
+for linha in range(len(NomeCurso1)):
+    if 'Semestre' in NomeCurso1[linha -1]:
+        lista_Completa.append(NomeCurso1[linha])
+
+
+
+lfv = {}
+teste = {}
+transf = str(lista_Completa)
+
+for linha in range(len(NomeCurso)):
+    if 'Semestre' in NomeCurso[linha]:
+        teste[linha] = {}
+        for linha2 in lista_Completa:
+            print(linha2)
+
+
+'''d = Lista_Final[v]
+h = Lista_horas[v]
+lfv[v] = {}
+lfv[v]["disciplinas"] = d
+lfv[v]['horas'] = h'''
