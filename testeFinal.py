6d8c89fe38bf53fb39ab4d89928d3a685906b6f0
Luckas-Ferreira <luckas0ferreira321@gmail.com>
2022-09-25 21:58:57 -0300

    asdf

--- testeFinal.py
+++ testeFinal.py
@@ -16,12 +16,39 @@ def CCurso(Dados, arquivo_TXT, linha):
         return Dados, curso
     return False
 
+def PProfesor(Dados, arquivo_TXT, linha):
+    if 'Vagas Oferecidas' in arquivo_TXT[linha]:
+        prof = arquivo_TXT[linha - 1].replace('\n', '')
+        professor = None
+        if 'Página' in prof:
+            trocar = prof.replace('Página', '-').replace('\n', '')
+            separar = trocar.split('-')
+            professor = separar[1] 
+        else:
+            professor = prof
+        if professor != None:
+            try:
+                Temp = Dados['Curso'][curso[1]]['Professores'][professor]['Disciplinas']
+            except:
+                Dados['Curso'][curso[1]]['Professores'][professor] = {}
+                Dados['Curso'][curso[1]]['Professores'][professor]['Disciplinas'] = {}
+            return curso, professor
+        return False
+    return False
+
 def DDiciplina(Dados, arquivo_TXT, linha):
     if 'Vagas Ocupadas:' in arquivo_TXT[linha]:
         disciplina = arquivo_TXT[linha].replace('\n', '').split('-')
         DiciName = disciplina[1].strip()
         Dados['Curso'][curso[1]]['Professores'][professor]['Disciplinas'][DiciName] = {}
         return DiciName, professor
+    return False
+
+def HHora(Dados, arquivo_TXT, linha):
+    if 'CH:' in arquivo_TXT[linha]:
+        horas = arquivo_TXT[linha].replace('CH:', '').split()
+        Dados['Curso'][curso[1]]['Professores'][professor]['Disciplinas'][DiciName] = horas[0]
+        return horas
     return False
 
 for linha in range(len(arquivo_TXT)):
@@ -32,22 +59,10 @@ for linha in range(len(arquivo_TXT)):
         curso = DadosCurso[1]
 
     #Encontrar todos os professores.
-    if 'Vagas Oferecidas' in arquivo_TXT[linha]:
-        prof = arquivo_TXT[linha - 1].replace('\n', '')
-        professor = None
-        if 'Página' in prof:
-            trocar = prof.replace('Página', '-').replace('\n', '')
-            separar = trocar.split('-')
-            professor = separar[1]
-            
-        else:
-            professor = prof
-        if professor != None:
-            try:
-                Temp = Dados['Curso'][curso[1]]['Professores'][professor]['Disciplinas']
-            except:
-                Dados['Curso'][curso[1]]['Professores'][professor] = {}
-                Dados['Curso'][curso[1]]['Professores'][professor]['Disciplinas'] = {}
+    DadosProfessor = PProfesor(Dados, arquivo_TXT, linha)
+    if DadosProfessor != False:
+        curso = DadosProfessor[0]
+        professor = DadosProfessor[1]
 
     #Encontrar todas as disciplinas.
     DadosDiciplina = DDiciplina(Dados, arquivo_TXT, linha)
@@ -56,9 +71,8 @@ for linha in range(len(arquivo_TXT)):
         professor = DadosDiciplina[1]
 
     #Encontrar carga horaria.
-    if 'CH:' in arquivo_TXT[linha]:
-        horas = arquivo_TXT[linha].replace('CH:', '').split()
-        Dados['Curso'][curso[1]]['Professores'][professor]['Disciplinas'][DiciName] = horas[0]
-
+    DadosHorario = HHora(Dados, arquivo_TXT, linha)
+    if DadosHorario != False:
+        hora = DadosHorario[0]
 
 print(json.dumps(Dados))
