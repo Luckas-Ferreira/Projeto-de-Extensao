733d6c909cabd7457abe5c8d1d6e508065f7432e
Luckas-Ferreira <luckas0ferreira321@gmail.com>
2022-10-21 12:52:24 -0300

    asdf

--- JavaScript/Codigo.js
+++ JavaScript/Codigo.js
@@ -1,4 +1,16 @@
-
-var nome = prompt('Qual teu nome? ')
-document.write(`Teu nome é ${nome}`)
-
+var pdfUtil = require('pdf-to-text');
+var pdf_path = "absolute_path/to/2022.1.pdf";
+ 
+//option to extract text from page 0 to 10
+var option = {from: 0, to: 10};
+ 
+pdfUtil.pdfToText(upload.path, option, function(err, data) {
+  if (err) throw(err);
+  console.log(data); //print text    
+});
+ 
+//Omit option to extract all text from the pdf file
+pdfUtil.pdfToText(upload.path, function(err, data) {
+  if (err) throw(err);
+  console.log(data); //print all text    
+});
--- JavaScript/Novo.js
+++ JavaScript/Novo.js
@@ -3,3 +3,4 @@ let l1 = [2, 5, 8, 9]
 console.log(l1)
 let l2 = l1.map((x)=>(x**2))
 console.log(l2)
+var pdf = pa
