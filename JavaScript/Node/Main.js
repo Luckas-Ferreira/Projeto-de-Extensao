3e3645bb9c7f77a39a943e3ac7a0e38d35206fda
Luckas-Ferreira <luckas0ferreira321@gmail.com>
2022-10-22 15:00:59 -0300

    l

--- JavaScript/Node/Main.js
+++ JavaScript/Node/Main.js
@@ -1,2 +1,14 @@
+const fs = require('fs');
+const arquivo = fs.readFileSync('Arquivos/Arquivo_TXT.txt').toString()
+let teste = arquivo.split()
+
+for (var i = 0; i <= 5; i++){
+    if ('Semestre' == teste[i]){
+        console.log('Tem essa porra!')
+    }else {
+        console.log(teste[i])
+        console.log('não tem esse caralho')
+    }
+}
 
 
