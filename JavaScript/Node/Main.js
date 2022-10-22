809dd7020ca9458be89d505b26eabd4d43b444f1
Luckas-Ferreira <luckas0ferreira321@gmail.com>
2022-10-22 19:40:54 -0300

    asdf

--- JavaScript/Node/Main.js
+++ JavaScript/Node/Main.js
@@ -1,13 +1,12 @@
 const fs = require('fs');
+const { stringify } = require('querystring');
 const arquivo = fs.readFileSync('Arquivos/Arquivo_TXT.txt').toString()
 let arquivo2 = arquivo.split('\n')
 
 Dados = {}
 Dados['Curso'] = {}
-Dados['Curso'] = {}
-teste = {}
-teste['Prof'] = {}
-for (var i = 0; i <= 6000; i++){
+
+for (var i = 0; i <= (arquivo2.length - 1); i++){
     //Pegar Curso
     if (arquivo2[i].indexOf('Semestre') !== -1){
         var curso = arquivo2[i].trim().split('-')
@@ -21,7 +20,7 @@ for (var i = 0; i <= 6000; i++){
     //Pegar nome professor
     if (arquivo2[i].indexOf('Vagas Ocupadas:') !== -1){
         var prof = arquivo2[i -1].split('  ')
-        professor = prof[prof.length - 1].trim()
+        var professor = prof[prof.length - 1].trim()
         try{
             Temp = Dados['Curso'][curso[1]]['Professor'][professor]['Disciplinas']
         }catch{
@@ -33,9 +32,8 @@ for (var i = 0; i <= 6000; i++){
     //Pegar nome disciplina
     if (arquivo2[i].indexOf('Vagas Ocupadas:') !== -1){
         var disc = arquivo2[i -1].split('  ')[0].trim()
-        disciplina = disc.split('- ')[1]
+        var disciplina = disc.split('- ')[1]
         Dados['Curso'][curso[1]]['Professor'][professor]['Disciplinas'][disciplina] = {}
-        
     }
     //Pegar carga horaria
     if (arquivo2[i].indexOf('CH:') !== -1){
@@ -44,5 +42,4 @@ for (var i = 0; i <= 6000; i++){
         Dados['Curso'][curso[1]]['Professor'][professor]['Disciplinas'][disciplina] = horas
     }
 }
-
 console.log(Dados)
