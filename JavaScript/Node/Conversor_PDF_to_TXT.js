14b1895945ff057fe564e16c33029657f595e75c
Luckas-Ferreira <luckas0ferreira321@gmail.com>
2022-10-22 18:44:52 -0300

    Perto do fim

--- JavaScript/Node/Conversor_PDF_to_TXT.js
+++ JavaScript/Node/Conversor_PDF_to_TXT.js
@@ -2,10 +2,9 @@ const fs = require('fs');
 var pdfUtil = require('pdf-to-text');
 var pdf_path = "Arquivos/2022.1.pdf";
 
+
 pdfUtil.pdfToText(pdf_path, function(err, data) {
   if (err) throw(err);
-    fs.writeFile('Arquivos/Arquivo_TXT.txt', data, (err) => {
-      if (err) throw err;
-        console.log('O arquivo foi criado!')
-    });
+    
 });
+console.log(arquivo)
--- JavaScript/Node/Main.js
+++ JavaScript/Node/Main.js
@@ -1,14 +1,48 @@
 const fs = require('fs');
 const arquivo = fs.readFileSync('Arquivos/Arquivo_TXT.txt').toString()
-let teste = arquivo.split()
+let arquivo2 = arquivo.split('\n')
 
-for (var i = 0; i <= 5; i++){
-    if ('Semestre' == teste[i]){
-        console.log('Tem essa porra!')
-    }else {
-        console.log(teste[i])
-        console.log('não tem esse caralho')
+Dados = {}
+Dados['Curso'] = {}
+Dados['Curso'] = {}
+teste = {}
+teste['Prof'] = {}
+for (var i = 0; i <= 6000; i++){
+    //Pegar Curso
+    if (arquivo2[i].indexOf('Semestre') !== -1){
+        var curso = arquivo2[i].trim().split('-')
+        try{
+            temp = Dados['Curso'][curso[1]]['Professor']
+        }catch{
+            Dados['Curso'][curso[1]] = {}
+            Dados['Curso'][curso[1]]['Professor'] = {}
+        }
+    }
+    //Pegar nome professor
+    if (arquivo2[i].indexOf('Vagas Ocupadas:') !== -1){
+        var prof = arquivo2[i -1].split('  ')
+        professor = prof[prof.length - 1].trim()
+        try{
+            Temp = Dados['Curso'][curso[1]]['Professor'][professor]['Disciplinas']
+        }catch{
+            Dados['Curso'][curso[1]]['Professor'][professor] = {}
+            Dados['Curso'][curso[1]]['Professor'][professor]['Disciplinas'] = {}
+        }
+        
+    }
+    //Pegar nome disciplina
+    if (arquivo2[i].indexOf('Vagas Ocupadas:') !== -1){
+        var disc = arquivo2[i -1].split('  ')[0].trim()
+        disciplina = disc.split('- ')[1]
+        Dados['Curso'][curso[1]]['Professor'][professor]['Disciplinas'][disciplina] = {}
+        
+    }
+    //Pegar carga horaria
+    if (arquivo2[i].indexOf('CH:') !== -1){
+        var hor = arquivo2[i].replace('CH:', '').replace(' horas', '').split('  ')
+        var horas = parseInt(hor[hor.length -1].trim())
+        Dados['Curso'][curso[1]]['Professor'][professor]['Disciplinas'][disciplina] = horas
     }
 }
 
-
+console.log(Dados)
