69cc9262717688e975572f68ebb06008191cc7d8
Luckas-Ferreira <luckas0ferreira321@gmail.com>
2022-10-23 17:30:26 -0300

    asdf

--- JavaScript/Angular/Projeto_Extensao/src/app/app.component.spec.ts
+++ JavaScript/Angular/Projeto_Extensao/src/app/app.component.spec.ts
@@ -23,7 +23,7 @@ describe('AppComponent', () => {
   it(`should have as title 'Projeto_Extensao'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Projeto_Extensao');
+    expect(app.upload).toEqual('Projeto_Extensao');
   });
 
   it('should render title', () => {
