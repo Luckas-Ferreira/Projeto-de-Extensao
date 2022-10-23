f6d911db4d75ba09d818bb52e535b21769acd96b
Luckas-Ferreira <luckas0ferreira321@gmail.com>
2022-10-23 14:59:20 -0300

    Primeiro projeto Angular

--- JavaScript/Angular/Projeto_Extensao/src/app/app.component.spec.ts
+++ JavaScript/Angular/Projeto_Extensao/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Projeto_extensao'`, () => {
+  it(`should have as title 'Projeto_Extensao'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Projeto_extensao');
+    expect(app.title).toEqual('Projeto_Extensao');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Projeto_extensao app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Projeto_Extensao app is running!');
   });
 });
