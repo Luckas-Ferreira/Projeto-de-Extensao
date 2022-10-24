57a3e7e2e7e47692929afd450129a5321f20b67f
Luckas-Ferreira <luckas0ferreira321@gmail.com>
2022-10-24 13:59:59 -0300

    Servidor rodando

--- JavaScript/Projeto/index.js
+++ JavaScript/Projeto/index.js
@@ -1,10 +1,28 @@
 const express = require('express');
 const app = express();
+const multer = require('multer');
 
-app.get('/', (req, res) => {
-    res.send("Bem Vind!");
+app.set('view engine', 'ejs');
+
+const storage = multer.diskStorage({
+    destination: function(req, file, cb){
+        cb(null, "uploads/")
+    },
+    filename: function(req, file, cb){
+        cb(null, file.originalname);
+    }
 })
 
+const upload = multer({storage})
+
+
+app.get('/', (req, res) => {
+    res.render("index");
+})
+ 
+app.post("/upload",upload.single("file"), (req, res) => {
+    res.send("Arquivo recebido!");
+})
 app.listen(8080, () => {
     console.log('Servidor rodando!');
 });
